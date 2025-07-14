7e4e2527b5286df52e807873811f0624d994d4f7
gt3002 <tulsi.goswami@quantumatix.in>
2025-07-14 18:10:58 +0530

    written solutions using cte window functions

--- sql_case_studies/chinkook_solutions.sql
+++ sql_case_studies/chinkook_solutions.sql
@@ -293,7 +293,163 @@ select invoiceid, total from Invoice i
 where total > all ( select total from invoice where InvoiceId != i.InvoiceId);
 
 --•	Get customers who have made more than 5 purchases using a correlated subquery.
+select c.CustomerId, c.FirstName
+from customer c where
+(select count(i.InvoiceId) from Invoice i 
+where i.CustomerId = c.CustomerId) > 5;
+
 --•	List tracks that appear in more than 2 playlists.
+select trackid, name from Track t where(
+select count(*)
+from PlaylistTrack pt
+where pt.TrackId = t.TrackId) > 2
+
 --•	Show albums where all tracks are longer than 3 minutes.
+select a.AlbumId, a.Title from Album a
+where not exists (
+select 1
+from track t
+where a.AlbumId = t.AlbumId and t.Milliseconds < 180000)
+
+--using not in
+select a.AlbumId, a.Title from Album a
+where a.albumid not in (
+select albumid
+from track t
+where a.AlbumId = t.AlbumId and t.Milliseconds <= 180000)
+
+--Window Functions
+--•	Rank customers by total spending.
+select CustomerId, sum(total) total_spend,
+rank() over(order by sum(total) desc) rnk
+from invoice
+group by CustomerId;
+
+--•	Show top 3 selling genres per country.
+with cte as(
+select i.BillingCountry, t.GenreId, sum(i.Total) ttl_sales
+from InvoiceLine il join invoice i on il.InvoiceId = i.InvoiceId
+join Track t on il.TrackId = t.TrackId
+group by i.BillingCountry, t.GenreId
+),--order by i.BillingCountry
+cte1 as(
+select BillingCountry, genreid, ttl_sales,
+rank() over(partition by billingcountry order by ttl_sales desc) rnk
+from cte c)
+
+select BillingCountry, g.Name as GenreName, ttl_sales Top3Sales
+from cte1 c join Genre g on c.GenreId = g.GenreId
+where c.rnk < 4
+
+--•	Get running total of invoice amounts by customer.
+select CustomerId, InvoiceId, Total, 
+sum(total) over(partition by customerid order by total rows between unbounded preceding and current row) running_total
+from Invoice
+
+--•	Find the invoice with the highest amount per customer.
+select invoiceid, customerid, total as highest_amount from(
+select InvoiceId, CustomerId, total,
+rank() over(partition by customerid order by total desc) rnk
+from invoice) as rnk_customers
+where rnk = 1;
+
+--•	Get the dense rank of employees by hire date.
+select (FirstName + ' '+ LastName) EmpName, Hiredate,
+DENSE_RANK() over(order by hiredate) drnk_by_hire_date
+from Employee
+
+--•	List tracks along with their rank based on unit price within each genre.
+select Name as track_name, genreid, unitprice,
+rank() over(partition by genreid order by unitprice) rnk_track
+from track;
+
+--•	Compute average invoice total by country using window functions.
+select distinct BillingCountry, avg(total) over(partition by billingcountry) avg_invoice_total
+from invoice;
+
+--•	Show lag/lead of invoice totals per customer.
+select InvoiceId, customerid, total, lag(total) over(partition by customerid order by total) pre_total,
+lead(total) over(partition by customerid order by total) post_total
+from Invoice;
+
+--•	List customers and their second highest invoice.
+select customerid, firstname+ ' ' + lastname as CustomerName, invoiceid, total from(
+select i.invoiceid, i.CustomerId, c.FirstName, c.LastName, total,
+rank() over(partition by i.customerid order by total desc) rnk
+from invoice i join Customer c on i.CustomerId = c.CustomerId) as rnk_customer_invoice
+where rnk = 2;
+
+--•	Get the difference in invoice total from previous invoice for each customer.
+select InvoiceId, customerid, total, 
+(total - isnull(lag(total) over(partition by customerid order by total),0)) diff_invoice_total
+from Invoice;
+
+--CTEs and Recursive Queries
+--•	List employees and their managers using recursive CTE.
+with cte as(
+select e.EmployeeId, e.FirstName, e.ReportsTo, cast(null as nvarchar(20)) as manager_name, 0 as level
+from Employee e
+where e.ReportsTo is null
+union all
+select e1.employeeid, e1.firstname, e1.ReportsTo, e2.FirstName, level+ 1 
+from Employee e1 join cte c on e1.ReportsTo = c.EmployeeId
+join Employee e2 on e1.ReportsTo = e2.EmployeeId
+)
+select * from cte;
+
+--•	Use CTE to get top 3 customers by total spending.
+with cte as(
+select top 3 customerid, sum(total) total_spend
+from Invoice i
+group by CustomerId
+order by total_spend desc)
+select c.customerid, (ct.FirstName + ' ' + ct.lastname) customer_name, total_spend
+from cte c join Customer ct on c.CustomerId = ct.CustomerId;
+
+--•	Create a CTE to list all invoice lines for albums by 'Metallica'.
+with cte as(
+select il.InvoiceLineId, il.InvoiceId, al.Title, al.ArtistId
+from InvoiceLine il join Track t on il.TrackId = t.TrackId
+join Album al on t.AlbumId =  al.AlbumId)
+
+select InvoiceId, InvoiceLineId, title as AlbumName
+from cte c join Artist a on c.ArtistId = a.ArtistId
+where a.Name = 'Metallica'
+order by Title
+
+--•	Use a CTE to show all tracks that appear in more than one playlist.
+with cte as(
+select t.TrackId, t.Name, count(distinct pt.PlaylistId) playlist_count
+from track t join PlaylistTrack pt on t.TrackId = pt.TrackId
+group by t.TrackId, t.Name)
+select * from cte
+where playlist_count > 1;
+
+--•	Recursive CTE to list employee hierarchy (if > 2 levels).
+with cte as(
+select e.EmployeeId, e.FirstName, e.ReportsTo, cast(null as nvarchar(50)) as emp_hierarchy, 0 as level
+from Employee e
+where e.ReportsTo is null
+union all
+select e1.employeeid, e1.firstname, e1.ReportsTo, 
+cast(concat(emp_hierarchy , '->' , e2.FirstName) as nvarchar(50)) emp_hierarchy , level+ 1 
+from Employee e1 join cte c on e1.ReportsTo = c.EmployeeId
+join Employee e2 on e1.ReportsTo = e2.EmployeeId
+)
+select * from cte where level >= 2;
+
+--•	CTE to get all albums with total track time > 30 minutes.
+with cte as(
+select AlbumId, sum(Milliseconds) total_track_time_ms
+from track
+group by AlbumId
+having sum(Milliseconds) > 1800000)
+select a.Title, (total_track_time_ms/60000) as track_time_in_minutes
+from cte c join Album a on c.AlbumId = a.AlbumId;
+
+--•	Get top 5 albums by total revenue using CTE and window functions.
 
 
+--•	Use CTE to find average track price per genre and filter only those above global average.
+--•	CTE to find customers with the longest names.
+--•	Create a CTE to rank all albums by number of tracks.
